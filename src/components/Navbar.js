2eb5450c73ac1414b78f9f91ab62b4b31c1ec746
Gizzhy <gizzhyakinz@gmail.com>
2023-06-02 11:33:02 +0100

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,23 +29,23 @@ export const Navbar = () => {
     return (
       <nav class="navbar navbar-expand-lg navbar-light sticky-top">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img className="logo" src={logo} alt="Logo"/></a>
+        <a class="navbar-brand" href="https://techmavendev.netlify.app/"><img className="logo" src={logo} alt="Logo"/></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link" href="#about">About</a>
+              <a class="nav-link" href="https://techmavendev.netlify.app/">About</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#contact">Services</a>
+              <a class="nav-link" href="#services">Services</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#about">Portfolio</a>
+              <a class="nav-link" href="https://techmavendev.netlify.app/">Portfolio</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#contact">Blog</a>
+              <a class="nav-link" href="https://techmavendev.netlify.app/">Blog</a>
             </li>
           </ul>
           <button class="btn btn-primary">Be in touch</button>
